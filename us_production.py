b264ba83bd738ececd4e44a643555439a32e547a
Mike DeLong <mikedelong@outlook.com>
2020-10-13 09:40:12 -0400

    feature: introduce Prophet and train the model using our data

--- us_production.py
+++ us_production.py
@@ -12,7 +12,8 @@ from matplotlib.pyplot import show
 from pandas import read_excel
 from pandas.plotting import register_matplotlib_converters
 from seaborn import lineplot
-
+from fbprophet import Prophet
+from pandas import to_datetime
 
 if __name__ == '__main__':
     time_start = time()
@@ -32,8 +33,10 @@ if __name__ == '__main__':
     url = 'https://www.eia.gov/dnav/pet/hist_xls/WCRFPUS2w.xls'
     sheet_name = 'Data 1'
     df = read_excel(io=url, names=['date', 'value'], sheet_name=sheet_name, skiprows=2, )
+    df['date'] = df['date'].apply(to_datetime)
     logger.info(df.shape)
+    model = Prophet()
+    model.fit(df=df.rename(columns={'date': 'ds', 'value': 'y'}, ))
     lineplot(x='date', y='value', data=df)
-    show()
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
