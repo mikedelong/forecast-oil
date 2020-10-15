fbf1d2388965d668115628edb4c5b0bfdc5c8707
Mike DeLong <mikedelong@outlook.com>
2020-10-15 10:20:26 -0400

    bug fix: get log file name right

--- opec_production.py
+++ opec_production.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     LOG_PATH = Path('./logs/')
     LOG_PATH.mkdir(exist_ok=True)
     log_file = str(LOG_PATH / 'log-{}-{}.log'.format(datetime.datetime.today().strftime('%Y-%m-%d_%H-%M-%S'),
-                                                     'us_production'))
+                                                     'opec_production'))
     format_ = '%(asctime)s : %(levelname)s : %(name)s : %(message)s'
     handlers_ = [FileHandler(log_file, encoding='utf-8', ), StreamHandler(stdout)]
     log_level_ = INFO
@@ -24,6 +24,5 @@ if __name__ == '__main__':
     basicConfig(datefmt='%m-%d-%Y %H:%M:%S', format=format_, handlers=handlers_, level=log_level_, )
     logger = getLogger(__name__)
     logger.info('started')
-
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
