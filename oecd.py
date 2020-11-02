4af340fce40f93e601b5da70fba2a32478a98902
Mike DeLong <mikedelong@outlook.com>
2020-11-02 08:22:18 -0500

    feature: report the flag codes and indicators in a loop rather than having special code per column

--- oecd.py
+++ oecd.py
@@ -31,8 +31,8 @@ if __name__ == '__main__':
     logger.info(df.shape)
     logger.info(df['LOCATION'].value_counts().to_dict())
     logger.info('we have {} unique locations'.format(df['LOCATION'].nunique()))
-    logger.info('indicator values are {}'.format(df['INDICATOR'].unique().tolist()))
-    logger.info('flag code values are {}'.format(df['Flag Codes'].unique().tolist()))
+    for column in ['Flag Codes', 'INDICATOR']:
+        logger.info('{} values are {}'.format(column, df[column].unique().tolist()))
     locations = sorted(df['LOCATION'].unique(), reverse=False, )
     # todo need to handle OECD and EU28 and G20 differently because they are aggregates
     excluded_locations = {'EU28', 'G20', 'OECD'}
