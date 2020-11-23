383352133a93344b211a12cba28c4f82e4dd08a6
Mike DeLong <mikedelong@outlook.com>
2020-11-23 11:46:26 -0500

    feature: use the new input data

--- oecd.py
+++ oecd.py
@@ -28,7 +28,8 @@ if __name__ == '__main__':
 
     # OECD data from https://data.oecd.org/energy/crude-oil-production.htm
     # input_file = './data/DP_LIVE_19102020150254641.csv'
-    input_file = './data/DP_LIVE_09112020172606257.csv'
+    # input_file = './data/DP_LIVE_09112020172606257.csv'
+    input_file = './data/DP_LIVE_23112020174032843.csv'
     df = read_csv(filepath_or_buffer=input_file)
     logger.info(df.shape)
     logger.info(df['LOCATION'].value_counts().to_dict())
